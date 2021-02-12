5c2ef89b0a0bcc728ef1e58b47b42c87a1290b0e
Ilia Konnov <iliakonnov@gmail.com>
2021-02-12 14:43:08 +0300

    Create empty project for TUI

---
+++ Projecto.Tui/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Projecto.Tui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var project = new Project();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
